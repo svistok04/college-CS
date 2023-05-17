cdc492aa4cf46735021d8f39b257a9511673f136
svistok04 <trynabebetter.everyday@gmail.com>
2023-05-17 10:27:34 +0200

    Lab 9 added

---
+++ semester-2/programming/labs/lab9/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+int main() {
+    std::vector<int> v{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+
+    sort(v.begin(), v.end());
+
+    std::cout << "Sorted \n";
+    for (auto x : v)
+        std::cout << x << " ";
+
+    std::vector<int> w{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+
+    //sort(w.begin(), w.end(), std::greater<>());
+    sort(w.begin(), w.end(), std::greater<>());
+    std::cout << "\nSorted \n";
+    for (auto x : w)
+        std::cout << x << " ";
+
+    return 0;
+}
+
+
