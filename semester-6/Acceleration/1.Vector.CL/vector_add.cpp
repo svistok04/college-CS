ca6f23ee9f95769ad702d6a67ff7aacf79a55b06
svistok04 <svstk@proton.me>
2025-05-09 11:48:25 +0200

    Vector Add code clean up + add SVM + move to another dir

--- semester-6/Acceleration/1.Vector.CL/vector_add.cpp
+++ semester-6/Acceleration/1.Vector.CL/vector_add.cpp
@@ -1,16 +1,14 @@
 #include <CL/cl_platform.h>
 #include <cctype>
 #include <cstddef>
-#include <cstdint>
 #include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
 #include <CL/cl.h>
-#endif
+
+// TODO attempt CL_MEM_USE_HOST_PTR if no SVM available and verify whether it assures zero-copy
+#define MB_INT_BUFFER_ALLOC_CAP 1250UL * sizeof(int) * 1024 * 1024
 
 #define WARN_CL_RELEASE(retval, what) \
 do { if ((retval) != CL_SUCCESS) { \
@@ -60,7 +58,8 @@ void list_device_info(cl_device_id device);
 void list_available_devices(device_entry* devices, cl_uint num_devices);
 void menu();
 void run_vector_add_on_device(cl_device_id dev, cl_ulong vector_len);
-cl_ulong get_max_vector_len(cl_device_id dev);
+size_t get_max_vector_len(cl_device_id dev, size_t element_size);
+size_t round_up(size_t val, size_t multiple); 
 size_t read_size_t_safely(const char* prompt);
 //program wykonywany na HOSCIE - host program
 int main(void) {
@@ -275,6 +274,15 @@ void list_device_info(cl_device_id device) {
   print_device_info_str(device, CL_DRIVER_VERSION, "Driver version");
   print_device_info_str(device, CL_DEVICE_VERSION, "Device version");
   print_device_info_str(device, CL_DEVICE_OPENCL_C_VERSION, "OpenCL C version");
+
+  cl_device_svm_capabilities svm_caps = 0;
+  cl_int err = clGetDeviceInfo(device, CL_DEVICE_SVM_CAPABILITIES, sizeof(svm_caps), &svm_caps, NULL);
+  if (err == CL_SUCCESS && svm_caps != 0) {
+    printf("SVM supported! Caps = 0x%lx\n", svm_caps);
+  } else {
+    printf("SVM not supported or OpenCL < 2.0\n");
+  }
+  // print_device_info_str(device, CL_DEVICE_SVM_CAPABILITIES, "svm");
   for (cl_uint i = 0; i < total_devices; ++i) {
     if (devices[i].device == device) {
       print_platform_info_str(devices[i].parent_platform, CL_PLATFORM_NAME, "Parent platform");
@@ -349,13 +357,17 @@ void menu() {
           if (sub_choice == 1) {
 
             for (cl_uint i = 0; i < total_devices; ++i) {
-              cl_ulong max_len = get_max_vector_len(devices[i].device);
-              cl_ulong actual_len = (user_len == 0 | user_len > max_len) ? max_len : user_len; 
+              size_t max_len = get_max_vector_len(devices[i].device, sizeof(int));
+              size_t actual_len = (user_len == 0 || user_len > max_len)
+                ? max_len
+                : round_up(user_len, 64);  // or round down if you prefer strict caps
+
               if (user_len > max_len) {
-                printf("Device %d: requested size %zu too large, using max %zu instead", i, user_len, max_len);
+                printf("Device %d: requested size %zu too large, using max %zu instead\n",
+                       i, user_len, max_len);
               } else if (user_len % 64 != 0) {
-                printf("Warning: vector length %zu is not divisible by local work-group size %zu.\n", user_len, local_item_size);
-                printf("Consider rounding up to a multiple of 64 or use 0 for max.\n\n");
+                printf("Warning: vector length %zu is not divisible by local work-group size 64.\n", user_len);
+                printf("Rounding up to %zu.\n\n", actual_len);
               }
               run_vector_add_on_device(devices[i].device, actual_len);
             }
@@ -364,14 +376,17 @@ void menu() {
           } else if (sub_choice >= 2 && sub_choice < (total_devices + 2)) {
             cl_uint target = sub_choice - 2;
             printf("Running test on device %u...\n", target);
-            cl_ulong max_len = get_max_vector_len(devices[target].device);
-            cl_ulong actual_len = (user_len == 0 | user_len > max_len) ? max_len : user_len;
-            if (user_len > max_len) {
+            size_t max_len = get_max_vector_len(devices[target].device, sizeof(int));
+              size_t actual_len = (user_len == 0 || user_len > max_len)
+                ? max_len
+                : round_up(user_len, 64);  // or round down if you prefer strict caps
+
+              if (user_len > max_len) {
               printf("Requested size %zu too large, using max %zu instead\n", user_len, max_len);
-            } else if (user_len % 64 != 0) {
-              printf("Warning: vector length %zu is not divisible by local work-group size %zu.\n", user_len, local_item_size);
-              printf("Consider rounding up to a multiple of 64 or use 0 for max.\n\n");
-            }
+              } else if (user_len % 64 != 0) {
+                printf("Warning: vector length %zu is not divisible by local work-group size 64.\n", user_len);
+                printf("Rounding up to %zu.\n\n", actual_len);
+              }
             run_vector_add_on_device(devices[target].device, actual_len);
             wait_for_enter();
             clear_screen();
@@ -401,36 +416,31 @@ void run_vector_add_on_device(cl_device_id dev, cl_ulong vector_len) {
 
   cl_ulong max_alloc;
   ret = clGetDeviceInfo(dev, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &max_alloc, NULL);
-  CHECK_CL_ERROR(ret, "Getting max allocation size");
-  size_t max_vector_len = max_alloc / (3 * sizeof(int));
-  if (max_vector_len > 32 * 1024 * 1024) {
-    max_vector_len = 32 * 1024 * 1024;
-  }
+  size_t max_vector_len = get_max_vector_len(dev, sizeof(int));
   size_t data_size = vector_len * sizeof(int);
 
   cl_context context = clCreateContext(NULL, 1, &dev, NULL, NULL, &ret);
   CHECK_CL_ERROR(ret, "Creating context");
   cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, dev, 0, &ret);
-  CHECK_CL_ERROR(ret, "Creating command queue");
-  cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, vector_len * sizeof(int), NULL, &ret);
-  CHECK_CL_ERROR(ret, "Creating buffer A");
-  cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, vector_len * sizeof(int), NULL, &ret);
-  CHECK_CL_ERROR(ret, "Creating buffer B");
-  cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, vector_len * sizeof(int), NULL, &ret);
-  CHECK_CL_ERROR(ret, "Creating buffer C");
+  // CHECK_CL_ERROR(ret, "Creating command queue");
+  // cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, vector_len * sizeof(int), NULL, &ret);
+  // CHECK_CL_ERROR(ret, "Creating buffer A");
+  // cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, vector_len * sizeof(int), NULL, &ret);
+  // CHECK_CL_ERROR(ret, "Creating buffer B");
+  // cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, vector_len * sizeof(int), NULL, &ret);
+  // CHECK_CL_ERROR(ret, "Creating buffer C");
 
-  int* A = (int*)malloc(data_size);
-  int* B = (int*)malloc(data_size);
-  int* C = (int*)malloc(data_size);
-
+  int* A = (int*)clSVMAlloc(context, CL_MEM_READ_ONLY, vector_len * sizeof(int), 0);
+  int* B = (int*)clSVMAlloc(context, CL_MEM_READ_ONLY, vector_len * sizeof(int), 0);
+  int* C = (int*)clSVMAlloc(context, CL_MEM_WRITE_ONLY, vector_len * sizeof(int), 0);
   for (size_t i = 0; i < vector_len; ++i) {
     A[i] = i;
     B[i] = vector_len - i;
   }
-  ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, vector_len * sizeof(int), A, 0, NULL, NULL);
-  RECORD_ERROR(ret, "Writing buffer A");
-  ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, vector_len * sizeof(int), B, 0, NULL, NULL);
-  RECORD_ERROR(ret, "Writing buffer B");
+  // ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, vector_len * sizeof(int), A, 0, NULL, NULL);
+  // RECORD_ERROR(ret, "Writing buffer A");
+  // ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, vector_len * sizeof(int), B, 0, NULL, NULL);
+  // RECORD_ERROR(ret, "Writing buffer B");
   const char* source = kernelSourceCode;
   const size_t source_size = strlen(kernelSourceCode);
   cl_program program = clCreateProgramWithSource(context, 1, &source, &source_size, &ret);
@@ -454,12 +464,15 @@ void run_vector_add_on_device(cl_device_id dev, cl_ulong vector_len) {
   cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
   RECORD_ERROR(ret, "Creating kernel");
 
-  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&a_mem_obj);
+  clSetKernelArgSVMPointer(kernel, 0, A);
   RECORD_ERROR(ret, "Setting kernel arg 0");
-  ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&b_mem_obj);
+  clSetKernelArgSVMPointer(kernel, 1, B);
   RECORD_ERROR(ret, "Setting kernel arg 1");
-  ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&c_mem_obj);
+  clSetKernelArgSVMPointer(kernel, 2, C);
   RECORD_ERROR(ret, "Setting kernel arg 2");
+  // ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&a_mem_obj);
+  // ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&b_mem_obj);
+  // ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&c_mem_obj);
   // ret = clSetKernelArg(kernel, 3, sizeof(int), (void*)&vector_len);
   // RECORD_ERROR(ret, "Setting kernel arg 3");
 
@@ -475,8 +488,8 @@ void run_vector_add_on_device(cl_device_id dev, cl_ulong vector_len) {
 
   clock_gettime(CLOCK_MONOTONIC, &end);
 
-  ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, vector_len * sizeof(int), C, 0, NULL, NULL);
-  RECORD_ERROR(ret, "Reading buffer C");
+  // ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, vector_len * sizeof(int), C, 0, NULL, NULL);
+  // RECORD_ERROR(ret, "Reading buffer C");
 
   // for(int i = 0; i < vector_len; i++)
   //printf("%d + %d = %d\n", A[i], B[i], C[i]);
@@ -489,30 +502,39 @@ void run_vector_add_on_device(cl_device_id dev, cl_ulong vector_len) {
   WARN_CL_RELEASE(ret, "kernel");
   ret = clReleaseProgram(program);
   WARN_CL_RELEASE(ret, "program");
-  ret = clReleaseMemObject(a_mem_obj);
+  clSVMFree(context, A);
+  // ret = clReleaseMemObject(a_mem_obj);
   WARN_CL_RELEASE(ret, "buffer A");
-  ret = clReleaseMemObject(b_mem_obj);
+  // ret = clReleaseMemObject(b_mem_obj);
+  clSVMFree(context, B);
   WARN_CL_RELEASE(ret, "buffer B");
-  ret = clReleaseMemObject(c_mem_obj);
+  // ret = clReleaseMemObject(c_mem_obj);
+  clSVMFree(context, C);
   WARN_CL_RELEASE(ret, "buffer C");
   ret = clReleaseCommandQueue(command_queue);
   WARN_CL_RELEASE(ret, "command queue");
   ret = clReleaseContext(context);
   WARN_CL_RELEASE(ret, "context");
-  free(A);
-  free(B);
-  free(C);
+  // free(A);
+  // free(B);
+  // free(C);
   if (first_error != CL_SUCCESS) {
     CHECK_CL_ERROR(first_error, error_context);
   }
 }
 
-cl_ulong get_max_vector_len(cl_device_id dev) {
+size_t get_max_vector_len(cl_device_id dev, size_t element_size) {
   cl_ulong max_alloc;
-  clGetDeviceInfo(dev, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &max_alloc, NULL);
-  cl_ulong safe_len = max_alloc / (3 * sizeof(int));
-  if (safe_len > 32 * 1024 * 1024) safe_len = 32 * 1024 * 1024;
-  return safe_len;
+  cl_int ret = clGetDeviceInfo(dev, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &max_alloc, NULL);
+
+  CHECK_CL_ERROR(ret, "Getting max allocation size");
+  cl_ulong safe_alloc = (MB_INT_BUFFER_ALLOC_CAP < max_alloc) ? MB_INT_BUFFER_ALLOC_CAP : max_alloc;
+
+  return safe_alloc / element_size;
+}
+
+size_t round_up(size_t val, size_t multiple) {
+  return (val + multiple - 1) / multiple * multiple;
 }
 
 size_t read_size_t_safely(const char* prompt) {
